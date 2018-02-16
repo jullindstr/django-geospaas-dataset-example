8e5331813e9e6fd4aade74744d6003b82a78173d
Julia Lindström <lindstrom.julia@gmail.com>
2018-02-16 21:39:15 +0000

    Updated json with coordinates

--- summary_app/views.py
+++ summary_app/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.core.serializers import serialize
-from geospaas.catalog.models import Dataset
-
+from geospaas.catalog.models import Dataset, GeographicLocation
+import json
 
 class DatasetView(generic.DetailView):
     model = Dataset
@@ -22,9 +22,16 @@ class IndexView(generic.ListView):
 
 def output_json(request,pk):
     data = Dataset.objects.all().filter(pk = pk)
-    result = serialize('geojson', data, geometry_field = 'polygon')
-    context = {'output':result}
-    #return render(request, 'summary_app/dataset_json.html', context, content_type = 'json')
-    return HttpResponse(result, content_type = 'json')
+    dataset_result = json.loads(serialize('geojson', data, geometry_field = 'geographic_location__geometry'))
+    gl = GeographicLocation.objects.filter(dataset=data)
+    gl_result = json.loads(serialize('geojson', gl, geometry_field='geometry'))
+    geom_dict = gl_result.get('features')[0]
+    dataset_result.update(geom_dict)
+    #context = {'Dataset json':dataset_result, 'GeographicLocation json':gl_result}
+    
+    return HttpResponse(json.dumps(dataset_result), content_type = 'json')
+    #context = {'output': dataset_result}     
+   # return render(request, 'summary_app/dataset_json.html', context, content_type = 'json')
+    
 
 	
