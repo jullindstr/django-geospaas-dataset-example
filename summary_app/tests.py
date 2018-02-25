b395aa6697ffad199144cbaec9b2d682eb16e67c
Julia Lindström <lindstrom.julia@gmail.com>
2018-02-25 22:37:26 +0000

    Fixtures and tests modifications

--- summary_app/tests.py
+++ summary_app/tests.py
@@ -1,43 +1,40 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import json 
-from django.test import TestCase
-
+from django.test import TestCase 
+from django.test.client import Client
 from django.core.urlresolvers import reverse
 
-from geospaas.catalog.models import Dataset, Source, GeographicLocation, Parameter, DatasetParameter, DatasetURI
-from django.conf import settings  
-from django.test import RequestFactory
+from geospaas.catalog.models import * 
 from geospaas.vocabularies.models import *
 
 
-class SummaryAppViewsTestCase(TestCase):
+
+class DatasetTableViewTestCase(TestCase):
     fixtures = ['summary_app/fixtures/vocabularies.json','summary_app/fixtures/catalog.json']
 
     def setUp(self):
 	self.ds = Dataset.objects.all().get(pk = 1)
     
-    def viewtest_dataset_table(self):
+    def test_view_dataset_table(self):
+	client = Client()
 	loc = GeographicLocation.objects.all().get(pk = 1)
 	uri = DatasetURI.objects.all().get(pk = 1)
 	resp = self.client.get('/summary_app/1/')
 	self.assertEqual(resp.status_code, 200)
-	self.assertEqual(resp.context['datasets'].pk, 1)
-	self.assertEquals(resp.context['datasets'].entry_title, 'Test dataset')
-	#self.assertTrue(str(ds.source) in resp.content)
-	#self.assertTrue(str(ds.ISO_topic_category) in resp.content)
-	#self.assertTrue(str(ds.data_center) in resp.content)
-	self.assertContains(resp, str(ds.data_center))
-	self.assertContains(resp, str(ds.cource))
-	self.assertContains(resp, str(ds.ISO_topic_category))
-	#self.assertContains(resp, str(ds.source.platform))
-	#self.assertTrue(str(ds.source.platform) in resp.content)
-	#self.assertTrue(str(ds.source.instrument) in resp.content)
-	self.assertEqual(str(ds.gcmd_location.category), Location.objects.all().get(pk = 1).category)
-	self.assertEqual(ds.dataseturi_set.get(pk = 1), uri)
-        self.assertEqual(ds.geographic_location.geometry.coords, loc.coords)
+	self.assertEqual(resp.context['dataset'].pk, 1)
+	self.assertTrue(str(self.ds.ISO_topic_category) in resp.content)
+	self.assertTrue(str(self.ds.data_center) in resp.content)
+	self.assertContains(resp, str(self.ds.data_center))
+	self.assertContains(resp, str(self.ds.ISO_topic_category))
+	self.assertContains(resp, str(self.ds.source.platform))
+	self.assertTrue(str(self.ds.source.platform) in resp.content)
+	self.assertTrue(str(self.ds.source.instrument) in resp.content)
+	#self.assertEqual(str(self.ds.gcmd_location.category), Location.objects.all().get(pk = 1).category)
+	self.assertEqual(self.ds.dataseturi_set.get(pk = 1), uri)
+        #self.assertEqual(self.ds.geographic_location.geometry, loc.coords)
 	self.assertTemplateUsed('dataset_table')
-	self.assertIn('<a href=""%s" >ds </a>' %reverse ("output_json"), response.content)
+	self.assertIn('<a href="%s">Json </a>' %reverse ('summary_app:output_json', kwargs = {'pk': 1}), resp.content)
 
 class IndexViewTestCase(TestCase):
 
--- summary_app/views.py
+++ summary_app/views.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from django.core.urlresolvers import reverse
 from django.views import generic
+
 from django.core.serializers import serialize
 from geospaas.catalog.models import Dataset, GeographicLocation
 import json
@@ -13,53 +12,42 @@ class DatasetView(generic.DetailView):
     template_name = 'summary_app/dataset_table.html'
     
 
-    def get_context_data(self, **kwargs):
-        context = super(DatasetView, self).get_context_data(**kwargs)
-        data = Dataset.objects.all().filter(pk = self.kwargs['pk'])
-	dataset_result = json.loads(serialize('geojson', data, geometry_field = 'geographic_location__geometry'))
-        gl = GeographicLocation.objects.filter(dataset=data)
-        gl_result = json.loads(serialize('geojson', gl, geometry_field='geometry'))
-        geom_dict = gl_result.get('features')[0]
-        dataset_result.update(geom_dict)
-        context['output'] = json.dumps(dataset_result)
-        return context
+    #def get_context_data(self, **kwargs):
+       # context = super(DatasetView, self).get_context_data(**kwargs)
+        #data = Dataset.objects.all().filter(pk = self.kwargs['pk'])
+	#dataset_result = json.loads(serialize('geojson', data, geometry_field = 'geographic_location__geometry'))
+        #gl = GeographicLocation.objects.filter(dataset=data)
+       # gl_result = json.loads(serialize('geojson', gl, geometry_field='geometry'))
+       # geom_dict = gl_result.get('features')[0]
+       # dataset_result.update(geom_dict)
+       # context['output'] =json.dumps(dataset_result)
+	#return context
 
-
+    
 
 class IndexView(generic.ListView): 	 
     model = Dataset
     context_object_name = 'datasets'
     template_name = 'summary_app/index.html'
     
-class JsonView(generic.View):
+
+class JsonView(generic.DetailView):
     template_name = 'summary_app/dataset_json.html'
     model = Dataset
 
     def get(self, request, pk):
-        #context = super(DatasetView, self).get_context_data(**kwargs)
         data = Dataset.objects.all().filter(pk = pk)
         dataset_result = json.loads(serialize('geojson', data, geometry_field = 'geographic_location__geometry'))
         gl = GeographicLocation.objects.filter(dataset=data)
         gl_result = json.loads(serialize('geojson', gl, geometry_field='geometry'))
         geom_dict = gl_result.get('features')[0]
         dataset_result.update(geom_dict)
-        #context['output'] = json.dumps(dataset_result)
-        return HttpResponse(json.dumps(dataset_result))
-
+        #context= {'output':json.dumps(dataset_result, sort_keys = True)}
+        return HttpResponse(json.dumps(dataset_result, sort_keys = True, indent = 4), content_type = 'json')
+	#return JsonResponse(context)
    
     
-def output_json(request,pk):
-    data = Dataset.objects.all().filter(pk= pk)
-    dataset_result = json.loads(serialize('geojson', data, geometry_field = 'geographic_location__geometry'))
-    gl = GeographicLocation.objects.filter(dataset=data)
-    gl_result = json.loads(serialize('geojson', gl, geometry_field='geometry'))
-    geom_dict = gl_result.get('features')[0]
-    dataset_result.update(geom_dict)
-    #context = {'Dataset json':dataset_result, 'GeographicLocation json':gl_result}
-    
-    return HttpResponse(json.dumps(dataset_result), content_type = 'json')
-    #context = {'output': dataset_result}     
-   # return render(request, 'summary_app/dataset_json.html', context, content_type = 'json')
+
     
 
 	
